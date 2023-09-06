9042944983b7d300f569802309dda9aae77a646f
Brett Curtis <brett@osinfra.io>
2023-09-06 19:53:54 -0400

    Enable Datadog integration (#18)

--- global/infra/main.tf
+++ global/infra/main.tf
@@ -5,6 +5,13 @@ terraform {
 
   required_providers {
 
+    # Datadog Provider
+    # https://registry.terraform.io/providers/DataDog/datadog/latest/docs
+
+    datadog = {
+      source = "datadog/datadog"
+    }
+
     # Google Cloud Platform Provider
     # https://registry.terraform.io/providers/hashicorp/google/latest/docs
 
@@ -14,11 +21,27 @@ terraform {
   }
 }
 
+provider "datadog" {
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+}
+
+# Datadog Google Cloud Platform Integration Module (osinfra.io)
+# https://github.com/osinfra-io/terraform-datadog-google-integration
+
+module "datadog" {
+  source = "github.com/osinfra-io/terraform-datadog-google-integration//global?ref=v0.1.0"
+
+  api_key         = var.datadog_api_key
+  is_cspm_enabled = true
+  project         = module.project.project_id
+}
+
 # Google Project Module (osinfra.io)
 # https://github.com/osinfra-io/terraform-google-project
 
 module "project" {
-  source = "github.com/osinfra-io/terraform-google-project?ref=v0.1.1"
+  source = "github.com/osinfra-io/terraform-google-project//global?ref=v0.1.2"
 
   billing_account                 = var.billing_account
   cis_2_2_logging_sink_project_id = var.cis_2_2_logging_sink_project_id
