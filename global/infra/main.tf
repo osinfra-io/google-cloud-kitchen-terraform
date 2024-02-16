49c1ee84525869ecc3a234397f720c3bf77faa9b
Brett Curtis <brett@osinfra.io>
2024-02-15 19:36:34 -0500

    Test project module fix

--- global/infra/main.tf
+++ global/infra/main.tf
@@ -50,7 +50,7 @@ provider "datadog" {
 # https://github.com/osinfra-io/terraform-google-project
 
 module "vpc_host_project" {
-  source = "github.com/osinfra-io/terraform-google-project//global?ref=brettcurtis/issue86"
+  source = "github.com/osinfra-io/terraform-google-project//global?ref=4faeb2ef30bfbb77e3bfd92460ebf17571102d2d"
 
   billing_account                 = var.billing_account
   cis_2_2_logging_sink_project_id = var.cis_2_2_logging_sink_project_id
@@ -85,7 +85,7 @@ module "vpc_host_project" {
 }
 
 module "gke_fleet_host_project" {
-  source = "github.com/osinfra-io/terraform-google-project//global?ref=brettcurtis/issue86"
+  source = "github.com/osinfra-io/terraform-google-project//global?ref=4faeb2ef30bfbb77e3bfd92460ebf17571102d2d"
 
   billing_account                 = var.billing_account
   cis_2_2_logging_sink_project_id = var.cis_2_2_logging_sink_project_id
@@ -125,7 +125,7 @@ module "gke_fleet_host_project" {
 }
 
 module "gke_fleet_member_project" {
-  source = "github.com/osinfra-io/terraform-google-project//global?ref=brettcurtis/issue86"
+  source = "github.com/osinfra-io/terraform-google-project//global?ref=4faeb2ef30bfbb77e3bfd92460ebf17571102d2d"
 
   billing_account                 = var.billing_account
   cis_2_2_logging_sink_project_id = var.cis_2_2_logging_sink_project_id
